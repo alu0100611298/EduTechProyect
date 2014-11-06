b29188cb4cbcf862f34244ac22db702bcf7f568c
alu0100611298 <alu0100611298@ull.edu.es>
2014-11-06 15:26:29 +0000

    test tabla user

--- test/test.rb
+++ test/test.rb
@@ -11,21 +11,25 @@ describe "shortened urls" do
 		@gclass = "gclass"
 		@gpromotion = "promotion"
 		#user
-		@name = "name"
-		@last_name = "last_name"
-		@username = "username"
-		@profile_picture = "profile_picture"
-		@created_at = ""
-
+		@uname = "name"
+		@ulast_name = "last_name"
+		@uusername = "username"
+		@uprofile_picture = "profile_picture"
+		@ucreated_at = ""
+		#Crear un grado
 		@table = Grade.first_or_create(:name => @gname, :class => @gclass, :promotion => @gpromotion)
 		@grade = Grade.first(:name => @gname)
-		#@user = User.new(:name => @name, :last_name => @last_name, :username => @username, :profile_picture => @profile_picture, :created_at => Time.now, :grade => @grade)
-		#@user.save
-		#@name = User.first(:name => @name)
+		#incluir usuarios en el grado
+		@user = User.first_or_create(:name => @uname, :last_name => @ulast_name, :username => @uusername, :profile_picture => @uprofile_picture, :created_at => Time.now, :grade => @grade)
+		@name = User.first(:name => @uname)
 	end
 
 	it "El nombre del grado es gname" do
 		assert_equal @gname, @grade.name
+	end
+
+	it "El usuario es name" do
+		assert_equal @uname, @name.name
 	end
 	
 end
