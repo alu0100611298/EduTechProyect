283b1851c1bdb49f19bbff4aea3fad3cb4b78b0e
Sergio Miguel Delgado <alu0100706468@ull.edu.es>
2014-12-18 22:39:16 +0000

    Añadidas rutas de registro

--- spec/edu_spec.rb
+++ spec/edu_spec.rb
@@ -15,3 +15,30 @@ def app
 	Sinatra::Application
 end
 
+describe " Edutech - funcionalidades de las rutas" do
+	it "get /" do
+		get '/'
+		expect(last_response).to be_ok
+	end
+
+	it "post de registro" do
+		post '/registro', :nombre => "Sergio", :apellidos => "Miguel", :pass => "123456", :pass1 => "123456", :sexo =>"masculino", :username => "sergio"
+		last_response.body['Tus últimas notas']
+	end
+
+	it "post de registro erroneo - contraseñas distintas" do
+		post '/registro', :nombre => "Sergio", :apellidos => "Miguel", :pass => "123456", :pass1 => "asdfgh", :sexo =>"masculino", :username => "sergio"
+		last_response.body['Las constraseñas son distintas']
+	end
+
+	it "post de registro erroneo - faltan campos" do
+		post '/registro', :nombre => "Sergio", :apellidos => "", :pass => "123456", :pass1 => "123456", :sexo =>"masculino", :username => "sergio"
+		last_response.body['Faltan datos en el registro']
+	end	
+
+	it "post /login" do
+		post '/login' , :user_log => "sergio", :pass_log => "123456"
+		last_response.body['Tus últimas notas']
+	end
+
+	
