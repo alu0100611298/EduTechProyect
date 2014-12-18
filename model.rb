a66c9d5a963f86c8ea052a679a26141189248667
Raquel Alvarez Hernandez <alu0100700435@ull.edu.es>
2014-12-18 20:26:28 +0000

    cambiando agrupacion de consulta

--- model.rb
+++ model.rb
@@ -58,7 +58,7 @@ class Game
   end
 
   def self.podio()
-    DataMapper.repository.adapter.select("SELECT user_username AS usuario, SUM(score) AS total_score FROM games GROUP BY user_id ORDER BY total_score DESC")
+    DataMapper.repository.adapter.select("SELECT user_username AS usuario, SUM(score) AS total_score FROM games GROUP BY user_username ORDER BY total_score DESC")
   end
 
   belongs_to  :user
